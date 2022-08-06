9c57612ab4afd873ce6c8883bef50fc3d2105237
unknown <daniel.silva3320@gmail.com>
2022-08-05 23:50:16 -0300

    fix: fix error initialized project

--- src/routes/middleware/AuthAdminUser.ts
+++ src/routes/middleware/AuthAdminUser.ts
@@ -1,17 +1,9 @@
 import { NextFunction, Request, Response } from "express";
 import jwt from "jsonwebtoken";
-import { Admin } from "../../model/Admin";
 
 export class AuthAdminUser {
     async execute(request: Request, response: Response, next: NextFunction) {
         const authHeader = request.headers.authorization;
-        // const { email } = request.body;
-        // const verifyAdmin = await Admin.findOne({ email });
-
-        // if (verifyAdmin.isAdmin === false) {
-        //     return response.status(401).json({ error: "unauthorized" });
-        // }
-
         const token = authHeader && authHeader.split(" ")[1];
 
         if (!token) {
