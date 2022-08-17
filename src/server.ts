a1860bc69ad373414dc2fe04f795abfc59405dfd
danielsilva33 <daniel.oliveiradasilvam@gmail.com>
2022-08-17 09:40:04 -0300

    remove user admin

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 import { app } from "./app";
 import { logger } from "./errors/Winston";
 
-app.listen(process.env.PORT, () => {
-    logger.info(`Server is running on PORT ${process.env.PORT}`);
+app.listen(process.env.PORT || 3333, () => {
+    logger.info(`Server is running on PORT ${process.env.PORT || 3333}`);
 });
