34f514eba4ca068317c74e6df95229beed9c94eb
unknown <daniel.silva3320@gmail.com>
2022-09-09 21:34:35 -0300

    fix: redis get variables from .env

--- src/database/redis/redisConfig.ts
+++ src/database/redis/redisConfig.ts
@@ -1,6 +1,13 @@
 import Redis from "ioredis";
 
-const redisClient = new Redis();
+const port = Number(process.env.PORT_REDIS);
+
+const redisClient = new Redis({
+    port: port,
+    host: process.env.HOST_REDIS,
+    username: process.env.USERNAME_REDIS,
+    password: process.env.PASSWORD_REDIS,
+});
 
 async function getRedis(value: string) {
     return await redisClient.get(value);
